0d985cb58732629e2a2d26d58d98c8ff59667314
hannah.kuo <hannah.kuo@servicenow.com>
2024-07-10 11:09:33 -0700

    add streamlit UI

---
+++ app.py
@@ -0,0 +1,41 @@
+import base64
+
+import pandas as pd
+import streamlit as st
+
+import eval
+
+
+@st.cache_data
+def process(file):
+    content_bytes = file.read()
+    return eval.analyze(content_bytes)
+
+
+def download_df(df, filename):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # Base 64 encode
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}.csv">Download CSV file</a>'
+    return href
+
+
+def main():
+    st.set_page_config(page_title="Evaluation Automation", page_icon=":chart_with_upwards_trend:")
+    st.header("Linguistic Error-checking Automation")
+
+    st.markdown("### Instructions")
+    st.markdown("1. Upload csv of data \n2. Click 'Process'\n3. Download the processed csv")
+
+    uploaded_file = st.file_uploader("Choose a file", type=["csv"], accept_multiple_files=False)
+
+    if st.button("Process"):
+        processed_data = process(uploaded_file)
+        st.markdown("---")
+        st.write("Evaluation complete!")
+        st.write(processed_data)
+
+        st.markdown(download_df(processed_data, 'output'), unsafe_allow_html=True)
+
+
+if __name__ == '__main__':
+    main()
