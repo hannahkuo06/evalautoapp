caf39db11801b25f60303658e43e32b1df0540bf
hannah.kuo <hannah.kuo@servicenow.com>
2024-07-15 12:21:32 -0700

    include info about taxonomy

--- app.py
+++ app.py
@@ -23,7 +23,11 @@ def main():
     st.markdown("### About")
     st.markdown("This tool is meant to automate the error-checking process in data evaluation. "
                 "It will process a runspec CSV and output the same CSV file with an extra column "
-                "\"Errors\" that includes a list of errors/bugs GPT4 found in the generated text.")
+                "\"Errors\" that includes a list of errors/bugs GPT4 found in the generated text."
+                "\n\n"
+                "There is an editable JSON file that creates a hierarchy of errors that are commonly"
+                "found during data evaluation. This way, we can manually include new errors for GPT4"
+                "to keep an eye out for in the data for more accurate/detailed error-checking.")
 
     st.markdown("### Instructions")
     st.markdown("1. Upload csv of data \n2. Click 'Process'\n3. View/Download the processed csv")
