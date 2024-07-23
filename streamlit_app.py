7de471ac413489735b9f047607b8f41c006c66f6
hannah.kuo <hannah.kuo@servicenow.com>
2024-07-23 16:31:58 -0700

    clean code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,4 @@
-import os
-import sys
-
 import streamlit as st
-
-module_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'app'))
-sys.path.insert(0, module_dir)
 
 st.set_page_config(page_title="Evaluation Automation",
                    page_icon=":chart_with_upwards_trend:",
