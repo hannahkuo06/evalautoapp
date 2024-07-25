2da2ed7b685eb2b17251faea9357a1d3bf66eb66
hannah.kuo <hannah.kuo@servicenow.com>
2024-07-25 14:03:59 -0700

    fix eval import

--- app/test_eval.py
+++ app/test_eval.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
 
-import app.eval
+import app.eval as eval
 
 
 def csv_to_bytes(file_path) -> bytes:
@@ -191,7 +191,7 @@ class TestParallelize(unittest.TestCase):
         self.assertNotIn('Justification', df.columns)
 
         file_bytes = csv_to_bytes('data/fake_data.csv')
-        df = eval.para_process(file_bytes)
+        df = eval.parallel_async(file_bytes)
         self.assertIn('Errors', df.columns)
         self.assertIn('Justification', df.columns)
 
