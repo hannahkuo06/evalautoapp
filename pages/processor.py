732075f2dfb866cff872d4cd97e86b4b4860e681
hannah.kuo <hannah.kuo@servicenow.com>
2024-07-25 15:24:17 -0700

    clean up

--- pages/processor.py
+++ pages/processor.py
@@ -3,25 +3,22 @@ import asyncio
 import streamlit as st
 import base64
 import time
-from app.eval import parallel_async
+from app.eval import parallel
 
 
 st.markdown("#### Instructions")
 st.markdown("1. Upload dataset CSV \n2. Click 'Process'\n3. View/Download the processed csv")
 
 
-# @st.cache_data
+@st.cache_data
 def process(file):
     start_time = time.perf_counter()
+
     content_bytes = file.read()
-    # asyncio.run(asyncio.get_event_loop().set_debug(True))
-    result_df = asyncio.run(parallel_async(content_bytes))
+    result_df = asyncio.run(parallel(content_bytes))
 
     end_time = time.perf_counter()
     st.write(f"Computation runtime: {end_time - start_time:.6f} seconds")
-
-    # loop = asyncio.get_event_loop()
-    # return loop.run_until_complete(parallel_async(content_bytes))
 
     return result_df
 
@@ -36,10 +33,9 @@ def download_df(df, filename):
 uploaded_file = st.file_uploader("Choose a file", type=["csv"], accept_multiple_files=False)
 
 if st.button("Process") and uploaded_file is not None:
-    df = process(uploaded_file)
+    with st.spinner("Processing your file..."):
+        df = process(uploaded_file)
     st.markdown("---")
     st.write("✅ Evaluation complete!")
     st.write("Hover over the below table, and the download icon will appear in the top right corner of the table.")
     st.data_editor(df)
-
-    # st.markdown(download_df(df, 'output'), unsafe_allow_html=True)
